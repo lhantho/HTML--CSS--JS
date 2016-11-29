3589072668bdb5e04f68c6ad44301d112ce3a0c6
Julien Leroy <kessdep@gmail.com>
2016-11-29 17:27:28 +0100

    quatrieme commit

--- js/js.js
+++ js/js.js
@@ -9,3 +9,12 @@ function contenuon() {
 	document.getElementById("1").style.visibility = "hidden" ;
 	document.getElementById("-1").style.visibility = "visible" ;
 }
+
+function changebackground() {
+
+	document.body.style.backgroundImage = "url('http://fullhdpictures.com/wp-content/uploads/2016/01/Great-Tulip-Wallpaper.jpg')";
+}
+function changebackground2() {
+
+	document.body.style.backgroundImage = "url('http://fullhdpictures.com/wp-content/uploads/2016/01/Great-Tulip-Wallpaper.jpg')";
+}
