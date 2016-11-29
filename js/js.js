55e2fcb09875d6243a91b202369d263a322f20b5
Julien Leroy <kessdep@gmail.com>
2016-11-29 14:57:20 +0100

    Mon deuxième commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function addcontenu() {
+	document.getElementById("0").style.visibility = "hidden" ;
+	document.getElementById("-1").style.visibility = "hidden" ;
+	document.getElementById("1").style.visibility = "visible" ;
+}
+
+function contenuon() {
+	document.getElementById("0").style.visibility = "visible" ;
+	document.getElementById("1").style.visibility = "hidden" ;
+	document.getElementById("-1").style.visibility = "visible" ;
+}
